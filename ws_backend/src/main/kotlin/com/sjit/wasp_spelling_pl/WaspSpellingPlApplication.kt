79a9a254e6fbe767e133c72bdbd512ba910b7efc
Nidan23 <dev.sulkowski@gmail.com>
2023-07-04 18:58:18 +0200

    [INIT]

---
+++ ws_backend/src/main/kotlin/com/sjit/wasp_spelling_pl/WaspSpellingPlApplication.kt
@@ -0,0 +1,11 @@
+package com.sjit.wasp_spelling_pl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WaspSpellingPlApplication
+
+fun main(args: Array<String>) {
+    runApplication<WaspSpellingPlApplication>(*args)
+}
---
+++ ws_backend/src/test/kotlin/com/sjit/wasp_spelling_pl/WaspSpellingPlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sjit.wasp_spelling_pl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WaspSpellingPlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
